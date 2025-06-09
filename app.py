2d1b5879091ddeba48ab8b9409f68a8e768f9df5
Sreyasree-001 <sreyasree202@gmail.com>
2025-06-10 01:15:24 +0530

    feat:Authentication

--- app.py
+++ app.py
@@ -2,21 +2,32 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import text 
-#import os
+
 from database.models import Product
 from routes.product_routes import product_bp
 from database.extension import db
 from routes.auth_routes import auth_bp
 
+from flask_jwt_extended import JWTManager, create_access_token
+from datetime import timedelta
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
 app = Flask(__name__)
-app.secret_key = 'supersecretkey'  # for session management
+app.secret_key = os.getenv('SUPER_SECRET_KEY')
 CORS(app)
 
 # MySQL credentials
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:Sreyasree27@127.0.0.1:3307/chatbotdb'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
+
+#JWT configure
+app.config["JWT_SECRET_KEY"] = os.getenv('SECRET_KEY')
+app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=1)
 
 # Register the blueprints
 app.register_blueprint(product_bp, url_prefix='/api')
@@ -34,8 +45,8 @@ def test_db():
 def home():
     return "E-commerce Chatbot Backend Running!"
 
-print("Registered routes:")
-print(app.url_map)
+# print("Registered routes:")
+# print(app.url_map)
 
 
 if __name__ == '__main__':
